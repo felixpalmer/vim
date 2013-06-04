f066f9859ccfc4259f5caa5b4a4246eee383f903
Felix Palmer <felixpalmer@gmail.com>
2013-06-04 10:42:29 +0100

    Typo

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-echo "Creating symlink for .vimrc in home dirctory"
+echo "Creating symlink for .vimrc in home directory"
 ln -s $DIR/.vimrc ~/.vimrc
 echo "Done!"
